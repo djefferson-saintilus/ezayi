b6333f10d436b2029512a320c36f99a71b38102e
Djefferson Saintilus <35054290+Zorkalel@users.noreply.github.com>
2020-02-08 21:52:32 -0500

    Add files via upload

---
+++ ezayi.py
@@ -0,0 +1,41 @@
+#coding:utf8
+#-------
+# Kal
+#------
+#importation locale
+#var vibration et message
+import os,sys,time,androidhelper
+dro=androidhelper.Android()
+import sendmail
+#message delivrer a screen
+#err=dro.makeToast("SUCCESS")
+#
+#vibration tel
+#vri=dro.vibrate(duration='100')
+#
+#captiver picture
+#cap=dro.cameraCapturePicture('/sdcard/spy.jpg')
+
+###code main ####
+#
+def calcul():
+    files=0
+    if(files == 0):
+        try:
+            with open("ip.txt",mode='a') as f :
+                print ("file created sucess")
+        except:
+            print("")
+        print("")
+        inc=files + 1
+        if(inc==1):
+            #insertion de data in files
+            os.system("ifconfig > ip.txt")
+            try:
+                with open('ip.txt',mode='r') as f:
+                    print(f.read())
+            except:
+                
+        else:
+            print("file not created")
+calcul()
