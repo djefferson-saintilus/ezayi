9edd7166be3c06baa4f4539dee48b41092b084c7
Djefferson Saintilus <35054290+Zorkalel@users.noreply.github.com>
2020-02-11 17:14:52 +0000

    Add files via upload
    Script  prix rechercher un mot particulier dans un fichier  texte

---
+++ main.py
@@ -0,0 +1,11 @@
+#coding:utf8
+
+print("Hello World")
+filecreated=open ('h.txt',mode='a')
+filenewline=filecreated.write ("Bonjour les gars")
+fileopen=open ('h.txt',mode='r')
+filereadlines=fileopen.readlines()
+if(filereadlines[1].find("cookie")):
+    print ("Un cookie Est présent")
+else :
+    print ("Pas de cookie présent")
