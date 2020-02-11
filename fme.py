833d6425374f1208f3e1a4aac61f6e478fe23214
Djefferson Saintilus <35054290+Zorkalel@users.noreply.github.com>
2020-02-11 17:21:46 +0000

    Update fme.py

--- fme.py
+++ fme.py
@@ -5,7 +5,7 @@ filecreated=open ('h.txt',mode='a')
 filenewline=filecreated.write ("Bonjour les gars")
 fileopen=open ('h.txt',mode='r')
 filereadlines=fileopen.readlines()
-if(filereadlines[1].find("cookie")):
+if(filereadlines[1].find("cookie")==True):
     print ("Un cookie Est présent")
 else :
     print ("Pas de cookie présent")
